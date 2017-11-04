f5d1b8b27c9cf37d1ee3b78a623ffd71e5281cf5
zhiyitianya <1564048529@qq.com>
2017-11-04 23:50:59 +0800

    创建spring4demo

--- spring-demo/src/main/java/com/test/spring/configuration/Config.java
+++ spring-demo/src/main/java/com/test/spring/configuration/Config.java
@@ -5,9 +5,11 @@ import com.test.spring.service.UseFunctionService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//此注解相当于spring容器的<beans>  容器的上下文
 @Configuration
 public class Config {
 
+    //bean注解一般在@Configuration配置类中声明  一般用作配置数据库
     @Bean
     public FunctionService functionService(){
         return new FunctionService();
