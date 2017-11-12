614748ce940cd8709b18454c1404c2f63f55bb6e
zhiyitianya <1564048529@qq.com>
2017-11-12 21:58:24 +0800

    合并自动配置module

--- springboot-autoConfigPlugin/src/main/java/com/pp/plugin/AutoConfigApplication.java
+++ springboot-autoConfigPlugin/src/main/java/com/pp/plugin/AutoConfigApplication.java
@@ -3,13 +3,9 @@ package com.pp.plugin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by shibiao on 2017/11/8.
- */
 @SpringBootApplication
-public class UseAutoApplication {
-
+public class AutoConfigApplication {
     public static void main(String[] args){
-        SpringApplication.run(UseAutoApplication.class,args);
+        SpringApplication.run(AutoConfigApplication.class,args);
     }
 }
