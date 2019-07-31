a4752d8489a6938a07e1d00bdb942a8dcf62648b
shibiao123 <zhiyitianya123>
2019-07-31 23:35:46 +0800

    feat: 增加slf4j logback日志

--- springboot-mybatis/src/main/java/com/test/boot/controller/HelloWorldController.java
+++ springboot-mybatis/src/main/java/com/test/boot/controller/HelloWorldController.java
@@ -1,12 +1,17 @@
 package com.test.boot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloWorldController {
+    private final static Logger log = LoggerFactory.getLogger(HelloWorldController.class);
     @RequestMapping("hello")
     public String hello(){
+        log.info("进入到hello方法");
         return "hello";
     }
 }
